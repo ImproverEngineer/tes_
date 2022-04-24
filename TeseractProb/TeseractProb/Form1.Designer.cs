f4c75b4e1466cce2aa85a2d47d87194d94e1c768
RinatMagnat <50412433+RinatMagnat@users.noreply.github.com>
2022-04-24 19:32:51 +0500

    2. create project
    2.1 создана программа подхватываюшая изображения

---
+++ TeseractProb/TeseractProb/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace TeseractProb
+{
+    partial class TsForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TsForm));
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.AddImage = new System.Windows.Forms.Button();
+            this.showText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture
+            // 
+            this.picture.Location = new System.Drawing.Point(12, 12);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(258, 258);
+            this.picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture.TabIndex = 0;
+            this.picture.TabStop = false;
+            // 
+            // AddImage
+            // 
+            this.AddImage.Location = new System.Drawing.Point(276, 12);
+            this.AddImage.Name = "AddImage";
+            this.AddImage.Size = new System.Drawing.Size(122, 29);
+            this.AddImage.TabIndex = 1;
+            this.AddImage.Text = "Добавить картинку";
+            this.AddImage.UseVisualStyleBackColor = true;
+            this.AddImage.Click += new System.EventHandler(this.AddImage_Click);
+            // 
+            // showText
+            // 
+            this.showText.Location = new System.Drawing.Point(12, 306);
+            this.showText.Multiline = true;
+            this.showText.Name = "showText";
+            this.showText.Size = new System.Drawing.Size(386, 138);
+            this.showText.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 287);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Распознаный текст";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(276, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Удалить картинку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 456);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.showText);
+            this.Controls.Add(this.AddImage);
+            this.Controls.Add(this.picture);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TsForm";
+            this.Text = "TeseractProb";
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.Button AddImage;
+        private System.Windows.Forms.TextBox showText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TeseractProb/TeseractProb/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TeseractProb
+{
+    public partial class TsForm : Form
+    {
+        private string FileName =""; 
+        public TsForm()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Добавить картинку на форму.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddImage_Click(object sender, EventArgs e)
+        {          
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Directory.GetCurrentDirectory() + @"\ImageViewText";
+            if (openFileDialog.ShowDialog() == DialogResult.OK){
+                
+                picture.Image = Image.FromFile(openFileDialog.FileName);
+            }
+            FileName = openFileDialog.FileName;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            picture.Image = null;
+            FileName = "";
+        }
+    }
+}
